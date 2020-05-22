624a8b3fef3665252304b55f038a5a04c6186279
HoneySehee <honeysehee@gmail.com>
2020-05-22 22:45:08 +0900

    ^^

---
+++ GreedyAlgorism/jungasuyol.py
@@ -0,0 +1,27 @@
+n = int(input())
+a = list(map,(int, input().split()))
+
+lt = 0
+rt = n - 1
+last = 0
+result = ""
+temp = []
+
+while lt <= rt:
+    if a[lt] > last:
+        temp.append((a[lt], 'L'))
+    if a [rt] > last:
+        temp.append((a[rt], 'R'))
+    temp.sort()
+    if len(temp) == 0:
+        break
+    else:
+        result = result + temp[0][1]
+        last = temp[0][0]
+        if temp[0][1] == 'L':
+            lt +=1
+        else : 
+            rt -=1
+    temp.clear()
+print(len(result))
+print(result)
